2b9f8605079b43e0833c543c89e540801c8ae62f
Daniel <heliriumdev@gmail.com>
2026-08-09 19:29:18 +0300

    fix: fix sha256 sum;

--- Formula/g8n.rb
+++ Formula/g8n.rb
@@ -2,7 +2,7 @@ class G8n < Formula
   desc "Type-safe environment struct generator for Go"
   homepage "https://github.com/reglyph/g8n"
   url "https://github.com/reglyph/g8n/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "1d5feee7b6bfe35bbe5272c82e6c8ce9ce9faf4f647859b51a3848af33f7191a"
   license "MIT"
 
   depends_on "go" => :build
